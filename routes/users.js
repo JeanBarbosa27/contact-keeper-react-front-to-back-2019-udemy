3b0ca58b8be2201992162ad9de9fa53e48aca541
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-06-14 14:13:48 -0300

    Implements user validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 const router = express.Router();
 
 // @route     POSt    api/auth
 // @desc      Register user
 // @access    Public
-router.post('/', (req, res) => {
-  res.send('Register user')
+router.post('/', [
+  check('name', 'Please add a name.').not().isEmpty(),
+  check('email', 'Please enter a valid e-mail.').isEmail(),
+  check(
+    'password',
+    'Please enter a password with at least 6 characters'
+  ).isLength({ min: 6}),
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.send(req.body);
 });
 
 module.exports = router;
