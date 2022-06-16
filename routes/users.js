951baaff45f3555328a6293bf9c1a08c0a3717fb
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-06-15 21:08:44 -0300

    feat: Encrypts user password and save user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
@@ -15,14 +16,38 @@ router.post('/', [
     'password',
     'Please enter a password with at least 6 characters'
   ).isLength({ min: 6}),
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  res.send(req.body);
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.find({ email });
+
+    if (!user) {
+      return res.status(400).json(
+        { errors: [{
+          param: 'email',
+          msg: `User with email ${email} already exists.`
+        }]}
+      )
+    }
+
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    res.send('User created');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ errors: [{ param: 'catch', msg: 'Server error' }]})
+  }
 });
 
 module.exports = router;
