15b4dab6ae28372b23646ad0e26cb2b7ef7dec11
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-07-05 18:55:38 -0300

    feat: add transition effects at contacts list

--- client/src/components/contacts/ContactsList.js
+++ client/src/components/contacts/ContactsList.js
@@ -1,10 +1,12 @@
 import { useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import contactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
 
 const ContactsList = () => {
   const { contacts, filtered } = useContext(contactContext);
+  const transitionTimeout = 500;
 
   if (!contacts.length) {
     return <h4>You should add contacts...</h4>;
@@ -18,17 +20,31 @@ const ContactsList = () => {
     <div>
       <h4>Contacts:</h4>
       <ul>
-        {filtered && filtered.length
-          ? filtered.map((contact) => (
-              <li key={contact.id}>
-                <ContactItem contact={contact} />
-              </li>
-            ))
-          : contacts.map((contact) => (
-              <li key={contact.id}>
-                <ContactItem contact={contact} />
-              </li>
-            ))}
+        <TransitionGroup>
+          {filtered && filtered.length
+            ? filtered.map((contact) => (
+                <CSSTransition
+                  key={contact.id}
+                  timeout={transitionTimeout}
+                  classNames="item"
+                >
+                  <li>
+                    <ContactItem contact={contact} />
+                  </li>
+                </CSSTransition>
+              ))
+            : contacts.map((contact) => (
+                <CSSTransition
+                  key={contact.id}
+                  timeout={transitionTimeout}
+                  classNames="item"
+                >
+                  <li>
+                    <ContactItem contact={contact} />
+                  </li>
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
       </ul>
     </div>
   );
