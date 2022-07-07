d8ebbb1ba194fc448d26c8daf4615aa453d6a458
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-07-06 22:52:19 -0300

    feat: implements alert context

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Contexts
-import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import ContactState from "./context/contact/ContactState";
 
 // Components
 import About from "./components/pages/About";
+import Alerts from "./components/alert/Alerts";
 import Home from "./components/pages/Home";
 import Login from "./components/auth/Login";
 import Navbar from "./components/layouts/Navbar";
@@ -18,10 +20,11 @@ function App() {
   return (
     <AuthState>
       <ContactState>
-        <Fragment>
+        <AlertState>
           <Router>
             <Navbar />
             <div className="container">
+              <Alerts />
               <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
@@ -30,7 +33,7 @@ function App() {
               </Routes>
             </div>
           </Router>
-        </Fragment>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,6 +1,6 @@
 // Alert
 export const SET_ALERT = "SET_ALERT";
-export const CLEAR_ALERT = "CLEAR_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
 
 // Authentication
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
