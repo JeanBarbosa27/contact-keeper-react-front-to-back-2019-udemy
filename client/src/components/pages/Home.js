c06f53a4b8cf847b610fa93a4ac9dc807de51f0d
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-07-07 17:21:38 -0300

    feat: redirect user to login, if haven't a token

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,27 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import authContext from "../../context/auth/authContext";
+
 import ContactForm from "../contacts/ContactForm";
 import ContactsList from "../contacts/ContactsList";
 import ContactFilter from "../contacts/ContactFilter";
 
 const Home = () => {
+  const { loadUser } = useContext(authContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const authToken = sessionStorage["contact-keeper-token"];
+    if (authToken) {
+      loadUser();
+    } else {
+      navigate("/login", { replace: true });
+    }
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="grid-2">
       <ContactForm />
